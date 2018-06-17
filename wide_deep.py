6f3a2fb225b1e95f5e71ee372dbfd6fc964b8b3a
MatsumasaH <matumasa2022@gmail.com>
2018-06-17 11:26:49 +0300

    Updated md file

--- wide_deep.py
+++ wide_deep.py
@@ -13,14 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 
-# This Command Will Work
-# python wide_deep.py --model_type=wide
-# python wide_deep.py --model_type=deep --export_dir=wide_deep_saved_model
-# tensorboard --logdir=csv_model
-# saved_model_cli show --dir=wide_deep_saved_model/1529182697/
-# saved_model_cli show --dir=wide_deep_saved_model/1529182697/ --tag_set serve --all
-# saved_model_cli show --dir=wide_deep_saved_model/1529182697/ --tag_set serve --signature_def="predict" --input_exprs='examples=[{"width":[100.], "height":[100.]}]'
-
 """Example code for TensorFlow Wide & Deep Tutorial using tf.estimator API."""
 from __future__ import absolute_import
 from __future__ import division
